f3d459445edc412c59232b277c51d93d711b0ab6
dcc00030 <dcasquel1995@gmail.com>
2017-02-09 19:11:55 +0100

    Added the tutorial with images

--- WhoRU/app/src/main/java/org/danico/whoru/IntroActivity.java
+++ WhoRU/app/src/main/java/org/danico/whoru/IntroActivity.java
@@ -1,5 +1,6 @@
 package org.danico.whoru;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,5 +13,9 @@ public class IntroActivity extends AppIntro {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addSlide(AppIntroFragment.newInstance(getString(R.string.appintro_title1), getString(R.string.appintro_description1), R.drawable.welcome, getResources().getColor(R.color.colorPrimary)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.appintro_title2), "", R.drawable.welcome, getResources().getColor(R.color.colorPrimary)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.appintro_title3), getString(R.string.appintro_description3), R.drawable.camera, getResources().getColor(R.color.colorPrimary)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.appintro_title4), getString(R.string.appintro_description4), R.drawable.check_tick, getResources().getColor(R.color.colorPrimary)));
+        askForPermissions(new String[]{Manifest.permission.CAMERA}, 2);
     }
 }
