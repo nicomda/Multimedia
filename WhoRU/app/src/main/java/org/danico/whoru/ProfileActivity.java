1d7ca96140ccd9416b742f312d730b6eb69f9902
Nicolás Moral <nicomda@hotmail.com>
2017-03-01 21:46:31 +0100

    Recycler Layout from Profile finished. Created ProfileAdapter to handle list view items. Changed collapsed toolbar color in Profile.

--- WhoRU/app/src/main/java/org/danico/whoru/ProfileActivity.java
+++ WhoRU/app/src/main/java/org/danico/whoru/ProfileActivity.java
@@ -4,10 +4,19 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<String> mDataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,17 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mRecyclerView=(RecyclerView) findViewById(R.id.profile_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager=new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mDataset=new ArrayList<>();
+        mDataset.add("Twitter");
+        mDataset.add("Website");
+        mDataset.add("Mail");
+        mAdapter=new ProfileAdapter(mDataset);
+        mRecyclerView.setAdapter(mAdapter);
 
 
     }
