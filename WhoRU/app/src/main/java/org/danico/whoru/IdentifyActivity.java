04bc09293546b2748f14443e90f4ae5bff550bfc
Nicolás Moral <nicomda@hotmail.com>
2017-03-02 19:01:20 +0100

    Lot of changes done in this commit. Identify Layout has been modified adding a frame layout that encloses everything. Other way I couldn't get right position in Z-Axis for the images used as buttons. Added some drawable resources and touched identify activity due to some camera permission issues

--- WhoRU/app/src/main/java/org/danico/whoru/IdentifyActivity.java
+++ WhoRU/app/src/main/java/org/danico/whoru/IdentifyActivity.java
@@ -5,91 +5,58 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.google.android.cameraview.CameraView;
 
 public class IdentifyActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
+    private CameraView camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identify);
-        //First launch tour
         launchTour();
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.identify_fab_capture);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                        */
                 Intent i = new Intent(getApplicationContext(), ProfileActivity.class);
                 startActivity(i);
             }
         });
+        camera = (CameraView) findViewById(R.id.camera);
+        //camera.start();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_identify, menu);
-        return true;
-    }
+    //TODO MAYBE WE NEED A SPLASH SCREEN DUE TO HIGH LOAD OF FIRST LAUNCH (JUST IN THE FIRST LAUNCH)
+    public void launchTour() {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        SharedPreferences getPrefs = PreferenceManager
+                .getDefaultSharedPreferences(getBaseContext());
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        //  Create a new boolean and preference and set it to true
+        boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+
+        //  If the activity has never started before...
+        if (isFirstStart) {
+
+            //  Launch app intro
+            Intent i = new Intent(getApplicationContext(), IntroActivity.class);
+            startActivity(i);
+
+            //  Make a new preferences editor
+            SharedPreferences.Editor e = getPrefs.edit();
+
+            //  Edit preference to make it false because we don't want this to run again
+            e.putBoolean("firstStart", false);
+
+            //  Apply changes
+            e.apply();
         }
 
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void launchTour(){
-
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                //  Initialize SharedPreferences
-                SharedPreferences getPrefs = PreferenceManager
-                        .getDefaultSharedPreferences(getBaseContext());
-
-                //  Create a new boolean and preference and set it to true
-                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
-
-                //  If the activity has never started before...
-                if (isFirstStart) {
-
-                    //  Launch app intro
-                    Intent i = new Intent(getApplicationContext(),IntroActivity.class);
-                    startActivity(i);
-
-                    //  Make a new preferences editor
-                    SharedPreferences.Editor e = getPrefs.edit();
-
-                    //  Edit preference to make it false because we don't want this to run again
-                    e.putBoolean("firstStart", false);
-
-                    //  Apply changes
-                    e.apply();
-                }
-            }
-        });
-
-        // Start the thread
-        t.start();
     }
 }
